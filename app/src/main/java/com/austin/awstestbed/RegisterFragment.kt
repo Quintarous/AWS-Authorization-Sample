b8f48de65e467db1a475dc98635815b33cb1fe06
Austin Green <austinoguy51@gmail.com>
2022-09-21 13:10:12 -0400

    - fragment_register.xml: New UI elements added. Including red error icons and red error labels for the password and email EditText views. These UI elements are hidden by default and will appear when an invalid password or email is entered. password must be at least 8 characters email must be a valid email address
    - RegisterFragment.kt: New addTextChangedListener lambdas are attached to the passwordEditText and the emailEditText. These listeners control the visibility of the new error UI elements.
    Coded logic to handle the submit button. We will cache the values from the username, password, and email EditText's and attempt to sign up a new user with AWS Cognito. If successful we immediately sign the user in with the same cached credentials. If sign in is successful UserData.loggedIn is set to true, a toast is displayed, and we navigate back to FirstFragment.
    If an error occurs either when signing up, or signing in a toast is displayed with the error message.
    
    - strings.xml/colors.xml/error.xml: Added new strings for the toast messages and error labels. Added a red color for the register fragments new error UI elements. Error.xml is the error icon being used.

--- app/src/main/java/com/austin/awstestbed/RegisterFragment.kt
+++ app/src/main/java/com/austin/awstestbed/RegisterFragment.kt
@@ -2,15 +2,23 @@ package com.austin.awstestbed
 
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.amplifyframework.auth.AuthException.UsernameExistsException
 import com.amplifyframework.auth.AuthUserAttributeKey
 import com.amplifyframework.auth.options.AuthSignUpOptions
 import com.amplifyframework.core.Amplify
 import com.austin.awstestbed.databinding.FragmentRegisterBinding
-
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+// TODO write tests for all of the registration flows!
 class RegisterFragment: Fragment() {
 
     private var _binding: FragmentRegisterBinding? = null
@@ -30,15 +38,98 @@ class RegisterFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.registerSubmitButton.setOnClickListener {
-            Log.i("bruh", "submit button pressed")
+            val username = binding.usernameEdittext.text.toString()
+            val password = binding.passwordEdittext.text.toString()
+            val email = binding.emailEdittext.text.toString()
 
-//            val options = AuthSignUpOptions.builder()
-//                .userAttribute(AuthUserAttributeKey.email(), "my@email.com")
-//                .build()
-//            Amplify.Auth.signUp("username2", "Password123", options,
-//                { Log.i("bruh", "Sign up succeeded: $it") },
-//                { Log.e ("bruh", "Sign up failed", it) }
-//            )
+            val options = AuthSignUpOptions.builder()
+                .userAttribute(AuthUserAttributeKey.email(), email)
+                .build()
+
+            Amplify.Auth.signUp(username, password, options,
+                // signup success
+                {
+                    // TODO sign in user, save username, show toast, navigate to first fragment
+                    // TODO research security for how to handle user password strings
+                    // TODO I can access username from a signup response. Figure out how to access username from a sign in response
+                    Log.i("bruh", "AuthSignUpResult: $it") // AuthSignupResult has a user field
+
+                    // signing the user in
+                    Amplify.Auth.signIn(username, password,
+                        {
+                            lifecycleScope.launch(Dispatchers.Main) {
+                                UserData.setLoggedInState(true) // setting the user to logged in
+                                // making a toast to confirm the user signed in
+                                Toast.makeText(
+                                    requireContext(),
+                                    getString(R.string.sign_in_successful_toast),
+                                    Toast.LENGTH_LONG
+                                ).show()
+
+                                findNavController().popBackStack() // navigating back to the first fragment
+                            }
+                        },
+                        { exception ->
+                            lifecycleScope.launch(Dispatchers.Main) {
+                                Toast.makeText(
+                                    requireContext(),
+                                    getString(R.string.an_error_occured, exception.localizedMessage),
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                    )
+                },
+
+                // signup failure
+                { authException ->
+                    lifecycleScope.launch(Dispatchers.Main) {
+                        // show a toast based on the exception type
+                        when (authException) {
+                            is UsernameExistsException -> {
+                                Toast.makeText(
+                                    requireContext(),
+                                    getString(R.string.username_already_exists),
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+
+                            else -> {
+                                Toast.makeText(
+                                    requireContext(),
+                                    getString(R.string.an_error_occured, authException.localizedMessage),
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                    }
+                }
+            )
+        }
+
+        // toggling the visibility of the password error icon and label
+        binding.passwordEdittext.addTextChangedListener {
+            // if the password is not long enough show an error
+            if (it.toString().length in 1..7) {
+                binding.passwordError.visibility = View.VISIBLE
+                binding.passwordErrorLabel.visibility = View.VISIBLE
+            } else { // else hide the error UI
+                binding.passwordError.visibility = View.GONE
+                binding.passwordErrorLabel.visibility = View.GONE
+            }
+        }
+
+        // toggling the visibility of the email error icon and label
+        binding.emailEdittext.addTextChangedListener {
+            val email = it.toString().trim() // trimming to eliminate whitespace
+            // if the email is not empty and doesn't match email format show an error
+            if (email.isNotEmpty() && !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.emailError.visibility = View.VISIBLE
+                binding.emailErrorLabel.visibility = View.VISIBLE
+            } else { // else hide the error UI
+                binding.emailError.visibility = View.GONE
+                binding.emailErrorLabel.visibility = View.GONE
+            }
         }
     }
 
